134c28330f10676c3a496773d92002b7a252ad54
teddy004 <tewodrosatirsaw4@gmail.com>
2022-09-16 11:52:14 +0000

    using puppet, creat a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file in /tmp.
+
+file { 'school':
+  name    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}

b9bd840b83be06f50ae5e761e967f42713d09afd
teddy004 <tewodrosatirsaw4@gmail.com>
2022-11-01 14:13:30 +0000

    strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes a faulty wordpress site
+exec { 'fix-wordpress':
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
